3cbcd37bcb11f511f0ac8d56949eeca4ffeb6ff0
Enzo Sandré <e.sandre@geosys.com>
2019-01-22 23:51:55 +0100

    Decimal extensions

---
+++ Lib.Reflection/DecimalExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Lib.Reflection
+{
+    /// <summary>
+    /// <see cref="decimal"/> extensions
+    /// </summary>
+    public static class DecimalExtensions
+    {
+        /// <summary>
+        /// Returns decimal number bytes
+        /// </summary>
+        public static byte[] GetBytes(this decimal dec) 
+            => decimal.GetBits(dec).SelectMany(BitConverter.GetBytes).ToArray();
+    }
+}
