5af6f85fea1c584f2f4aa20e4bf33c8a327839eb
Enzo Sandré <e.sandre@geosys.com>
2019-01-31 22:08:30 +0100

    Factories with multiple inputs

--- Lib.Patterns/IFactory.cs
+++ Lib.Patterns/IFactory.cs
@@ -1,7 +1,7 @@
 ﻿namespace Lib.Patterns
 {
     /// <summary>
-    /// A Factory
+    /// A Factory with one input
     /// </summary>
     public interface IFactory<in TInput, out TOutput>
     {
@@ -9,5 +9,27 @@
         /// Factories Output from Input
         /// </summary>
         TOutput Factory(TInput input);
+    }
+
+    /// <summary>
+    /// A Factory with two inputs
+    /// </summary>
+    public interface IFactory<in TInput1, in TInput2, out TOutput>
+    {
+        /// <summary>
+        /// Factories Output from Inputs
+        /// </summary>
+        TOutput Factory(TInput1 input1, TInput2 input2);
+    }
+
+    /// <summary>
+    /// A Factory with three inputs
+    /// </summary>
+    public interface IFactory<in TInput1, in TInput2, in TInput3, out TOutput>
+    {
+        /// <summary>
+        /// Factories Output from Inputs
+        /// </summary>
+        TOutput Factory(TInput1 input1, TInput2 input2, TInput3 input3);
     }
 }
