92e573fb2a6fc8690afb10144a1407cff69620bf
Enzo Sandré <e.sandre@geosys.com>
2019-04-09 14:21:20 +0200

    Bugfix on JsonResult with no JsonSerializer

--- Lib.Api/JsonResult.cs
+++ Lib.Api/JsonResult.cs
@@ -8,12 +8,8 @@ namespace Lib.Api
     /// </summary>
     public class JsonResult<TRepresented> : JsonResult
     {
-        internal JsonResult(JsonResult nonGeneric) : base((TRepresented) nonGeneric.Value, nonGeneric.SerializerSettings)
-        {
-        }
-
         /// <inheritdoc />
-        public JsonResult(TRepresented value) : base(value)
+        internal JsonResult(TRepresented value) : base(value)
         {
         }
 
@@ -41,12 +37,17 @@ namespace Lib.Api
         /// Extension that wraps <see cref="Controller"/> Json method
         /// </summary>
         public static JsonResult<TRepresented> Json<TRepresented>(this Controller controller, TRepresented value)
-            => new JsonResult<TRepresented>(controller.Json(value));
+        {
+            var innerResult = controller.Json(value);
 
-        /// <summary>
-        /// Extension that wraps <see cref="Controller"/> Json method with serializer settings
-        /// </summary>
-        public static JsonResult<TRepresented> Json<TRepresented>(this Controller controller, TRepresented value, JsonSerializerSettings serializerSettings)
-            => new JsonResult<TRepresented>(controller.Json(value, serializerSettings));
+            var result = new JsonResult<TRepresented>((TRepresented) innerResult.Value)
+            {
+                SerializerSettings = innerResult.SerializerSettings,
+                ContentType = innerResult.ContentType,
+                StatusCode = innerResult.StatusCode
+            };
+
+            return result;
+        }
     }
 }
