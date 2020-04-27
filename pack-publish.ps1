cba264149a14d82448c70d54374b58098e33e6e3
Enzo Sandré <enzo@enzosandre.fr>
2020-04-27 11:26:21 +0200

    No need to spam my server

--- pack-publish.ps1
+++ pack-publish.ps1
@@ -1,6 +1,6 @@
 dotnet clean Lib.All.sln
 dotnet build Lib.All.sln -c Release
 dotnet pack Lib.All.sln --include-symbols --include-source -p:SymbolPackageFormat=snupkg -o Release -c Release
-nuget push Release\*.nupkg -ApiKey R9385gmqDta6SkACSMnqSijjk3EK -Source https://baget.ludd.info/v3/index.json
+nuget push Release\*.nupkg -ApiKey NoNeedToSearchInOldCommitsItHasChanged -Source https://baget.ludd.info/v3/index.json
 Remove-Item Release\*
 Remove-Item Release\
