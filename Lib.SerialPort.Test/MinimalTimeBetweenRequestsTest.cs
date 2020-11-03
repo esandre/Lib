2be534b8aa4228d490be6ca6e541e0a9302d161d
Enzo Sandré <enzo@enzosandre.fr>
2020-11-03 21:49:14 +0100

    Suppression d'un second test superflu

--- Lib.SerialPort.Test/MinimalTimeBetweenRequestsTest.cs
+++ Lib.SerialPort.Test/MinimalTimeBetweenRequestsTest.cs
@@ -12,21 +12,6 @@ namespace Lib.SerialPort.Test
     public class MinimalTimeBetweenRequestsTest
     {
         [TestMethod]
-        public void Une_Requête_Unique_N_Est_Pas_Retardée()
-        {
-            var transceiver = Mock.Of<ISerialPortTransceiver>(
-                m => m.TransceiveAsync(It.IsAny<byte[]>(), It.IsAny<CancellationToken>()) == Task.FromResult(Enumerable.Empty<byte>())
-            );
-
-            var time = TimeSpan.FromSeconds(1);
-            var testedTransceiver = new MinimalTimeBetweenRequests(transceiver, time);
-
-            Check.ThatAsyncCode(
-                    async () => await testedTransceiver.TransceiveAsync(new byte[0], CancellationToken.None))
-                .LastsLessThan(time.TotalMilliseconds, TimeUnit.Milliseconds);
-        }
-
-        [TestMethod]
         public void Deux_Requêtes_Sont_Séparées_Par_Le_Temps_Minimal()
         {
             var transceiver = Mock.Of<ISerialPortTransceiver>(
