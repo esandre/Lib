594bbd51953a2f78ea83dfcd8258011412a0d066
Enzo Sandré <enzo@enzosandre.fr>
2020-11-24 12:05:07 +0100

    Meilleure verbosité

--- Lib.Encapsulation.String/RegexStringDecorator.cs
+++ Lib.Encapsulation.String/RegexStringDecorator.cs
@@ -7,17 +7,17 @@ namespace Lib.Encapsulation.String
     {
         public RegexStringDecorator(IEncapsulatedValue<string> other, Regex regex) : base(other)
         {
-            if (!regex.IsMatch(other.Value)) throw new FormatException(BadInputMessage(regex));
+            if (!regex.IsMatch(other.Value)) throw new FormatException(BadInputMessage(other.Value, regex));
         }
 
         public RegexStringDecorator(string input, Regex regex) : base(input)
         {
-            if (!regex.IsMatch(input)) throw new FormatException(BadInputMessage(regex));
+            if (!regex.IsMatch(input)) throw new FormatException(BadInputMessage(input, regex));
         }
 
         /// <summary>
         /// Message for when the input is not alphanumerical
         /// </summary>
-        private static string BadInputMessage(Regex regex) => $"Input doest not matches regex {regex}";
+        private static string BadInputMessage(string input, Regex regex) => $"Input {input} doest not matches regex {regex}";
     }
 }
