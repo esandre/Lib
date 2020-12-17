a2cb8a17845fc569649273b193e895241f6239fe
Enzo Sandré <enzo@enzosandre.fr>
2020-12-17 11:41:43 +0100

    Console cancellation token

---
+++ Lib.Console/ConsoleCancellationToken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Lib.Console
+{
+    public class ConsoleCancellationToken
+    {
+        public static CancellationToken SigtermCancellationToken
+        {
+            get
+            {
+                var cancellationTokenSource = new CancellationTokenSource();
+                AppDomain.CurrentDomain.ProcessExit +=
+                    (sender, args) => cancellationTokenSource.Cancel();
+                System.Console.CancelKeyPress  +=
+                    (sender, args) => cancellationTokenSource.Cancel();
+                return cancellationTokenSource.Token;
+            }
+        }
+    }
+}
